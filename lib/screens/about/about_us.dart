90decb5768766ed6bc7d45420681911a90556711
TaherSalah <tahersalah2016@gmail.com>
2023-02-26 01:35:46 +0200

    add social links and rating

--- lib/screens/about/about_us.dart
+++ lib/screens/about/about_us.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:new_task/shard/components/constance.dart';
 import 'package:new_task/shard/widgets/my_divider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({Key? key}) : super(key: key);
@@ -10,57 +13,240 @@ class AboutScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About Us'),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: Container(
-              height: 150,
-              width: 150,
-              child: Image.asset('assets/images/logo.png'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          Text('version : 1.0.0',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(fontSize: 13, color: Colors.grey)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.copyright_sharp, size: 20),
-              Text(
-                'All Rights Reseved by Dev Taher Salah \u{1f60e}',
+            Center(
+              child: Container(
+
+                height: 150,
+                width: 150,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+            ),
+            Text('version : 1.0.0',
                 style: Theme.of(context)
                     .textTheme
                     .bodyLarge!
-                    .copyWith(fontSize: 14),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 23.0, vertical: 12),
-            child: myDivider(),
-          ),
-          Directionality(
-            textDirection: TextDirection.rtl,
-            child: Column(
+                    .copyWith(fontSize: 13, color: Colors.grey)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                const Icon(Icons.copyright_sharp, size: 20),
                 Text(
-                  'عن البرنامج',
-                  style: Theme.of(context).textTheme.bodyLarge,
+                  'All Rights Reseved by Dev Taher Salah \u{1f60e}',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontSize: 14),
                 ),
-                const Text(
-                  textAlign:TextAlign.center,
-                    selectionColor: Colors.amber,
-                    'برنامج Note Tasks هو رفيقك الاول في تسجيل مهامك اليومية ومتابعتها بشكل مستمر فمن خلاله تستطيع تسجيل كل ماتريد من ملاحظات يومية بكل سهولة ويسر دون خوف من ضياع هذة الملاحظات في اي وقت بشكل او باخر اترك لك تجربة ممتعه لهذا البرنامج واذا كان لديك اي اقتراحات لتطوير هذا البرنامج قم بمراسلتي من خلال روابط السوشيال ميديا الموجودة اسفل هذة الصفحة.')
               ],
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 23.0, vertical: 12),
+              child: myDivider(),
+            ),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(child: myDivider()),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('عن البرنامج',style: Theme.of(context).textTheme.bodyLarge,),
+                        ),
+                        Expanded(child: myDivider()),
+                      ],
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                     Text(
+                        textAlign: TextAlign.justify,
+                        selectionColor: Colors.amber,
+                        'برنامج Note Tasks هو رفيقك الاول في تسجيل مهامك اليومية ومتابعتها بشكل مستمر فمن خلاله تستطيع تسجيل كل ماتريد من ملاحظات يومية بكل سهولة ويسر دون خوف من ضياع هذة الملاحظات في اي وقت بشكل او باخر اترك لك تجربة ممتعه لهذا البرنامج واذا كان لديك اي اقتراحات لتطوير هذا البرنامج قم بمراسلتي من خلال روابط السوشيال ميديا الموجودة اسفل هذة الصفحة.',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontSize: 16.5,fontWeight: FontWeight.normal),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(child: myDivider()),
+                        Padding(
+                          padding: const EdgeInsets.all(7.0),
+                          child: Text('تواصل معنا',style: Theme.of(context).textTheme.bodyLarge,),
+                        ),
+                        Expanded(child: myDivider()),
+                      ],
+                    ),
+                    Container(
+                        decoration:  BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.grey.withOpacity(0.7),
+                                ),
+                                left:BorderSide (
+                                  color: Colors.grey.withOpacity(0.7),
+                                ) ,
+                                right:BorderSide (
+                                  color: Colors.grey.withOpacity(0.7),
+                                ))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 18.0),
+                          child: const SocialIcons(),
+                        )),
+                    Row(
+                      children: [
+                        Expanded(child: myDivider()),
+                        Padding(
+                          padding: const EdgeInsets.all(7.0),
+                          child: Text('تقييم التطبيق',style: Theme.of(context).textTheme.bodyLarge,),
+                        ),
+                        Expanded(child: myDivider()),
+
+                      ],
+                    ),
+                    RetBar()
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class SocialIcons extends StatelessWidget {
+  const SocialIcons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(
+          onTap: () {
+            launchUrl(youtube, mode: LaunchMode.externalApplication);
+          },
+          child: Container(
+            child: Image.asset(
+              'assets/images/youtube.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        InkWell(
+          onTap: () {
+            launchUrl(facebook, mode: LaunchMode.externalApplication);
+          },
+          child: Container(
+            child: Image.asset(
+              'assets/images/facebook.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        InkWell(
+          onTap: () {
+            launchUrl(linkedin, mode: LaunchMode.externalApplication);
+          },
+          child: Container(
+            child: Image.asset(
+              'assets/images/linkedin.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        InkWell(
+          onTap: () {
+            launchUrl(whatsapp, mode: LaunchMode.externalApplication);
+          },
+          child: Container(
+            child: Image.asset(
+              'assets/images/whatsapp.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        InkWell(
+          onTap: () {
+            launchUrl(github, mode: LaunchMode.externalApplication);
+          },
+          child: Container(
+            child: Image.asset(
+              'assets/images/github.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        InkWell(
+          onTap: () {
+            launchUrl(insta, mode: LaunchMode.externalApplication);
+          },
+          child: Container(
+            child: Image.asset(
+              'assets/images/instagram.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ),
+
+
+      ],
+    );
+  }
+}
+class RetBar extends StatelessWidget {
+  const RetBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RatingBar.builder(
+      initialRating: 3,
+      minRating: 1,
+      direction: Axis.horizontal,
+      allowHalfRating: true,
+      itemCount: 5,
+      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+      itemBuilder: (context, _) => Icon(
+        Icons.star,
+        color: Colors.amber,
+      ),
+      onRatingUpdate: (rating) {
+        print(rating);
+      },
     );
   }
 }
--- lib/shard/components/constance.dart
+++ lib/shard/components/constance.dart
@@ -1,1 +1,19 @@
+import 'package:url_launcher/url_launcher.dart';
 
+
+ launchURL(String url) async {
+  final Uri uri = Uri(scheme: "https", host: url);
+  if(!await launchUrl(
+    uri,
+    mode: LaunchMode.externalApplication,
+  )) {
+    throw "Can not launch url";
+  }}
+
+final Uri phoneNumber=Uri.parse('tel:+0201094529752');
+final Uri whatsapp=Uri.parse('https://wa.me/01094529752');
+final Uri youtube=Uri.parse('https://www.youtube.com/channel/UCZZMcqVVtQSsA5dnQ6xN8nQ');
+final Uri facebook=Uri.parse('https://www.facebook.com/taher.salah.7927');
+final Uri linkedin=Uri.parse('https://www.linkedin.com/in/taher-salah-1a5622134/');
+final Uri github=Uri.parse('https://github.com/TaherSalah');
+final Uri insta=Uri.parse('https://www.instagram.com/tahersalahm/');
